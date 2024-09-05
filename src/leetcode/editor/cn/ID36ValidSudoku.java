144e6e68880f45e66746eead989a4a1736abf4b2
yu <yu@example.com>
2024-09-05 16:07:55 +0800

    School update commit 矩阵

--- src/leetcode/editor/cn/ID36ValidSudoku.java
+++ src/leetcode/editor/cn/ID36ValidSudoku.java
@@ -1,22 +1,81 @@
 package leetcode.editor.cn;
 
-import java.util.*;
-public class ID36ValidSudoku{
-	public static void main(String[] args) {
-		Solution solution = new ID36ValidSudoku().new Solution();
-		StringBuilder builder = new StringBuilder();
-		
-		//执行测试
-		
-		System.out.println(builder);		
-	}
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
+public class ID36ValidSudoku {
+    public static void main(String[] args) {
+        Solution solution = new ID36ValidSudoku().new Solution();
 
-		return true;
+        // 执行测试
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        solution.isValidSudoku(board);
+        System.out.println("");
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /*纯纯的举证遍历*/
+        public boolean isValidSudoku(char[][] board) {
+            int[] flag = new int[10];
+            int number = 0;
+            // 逐行
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (board[i][j] != '.') {
+                        number = board[i][j] - '0';
+                        flag[number]++;
+                        if (flag[number] > 1)
+                            return false;
+                    }
+                }
+                flag = new int[10];
+            }
+
+            // 逐列
+            flag = new int[10];
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (board[j][i] != '.') {
+                        number = board[j][i] - '0';
+                        flag[number]++;
+                        if (flag[number] > 1)
+                            return false;
+                    }
+                }
+                flag = new int[10];
+            }
+
+            // 3×3宫格
+            flag = new int[10];
+            for (int r = 0; r < 9; ) {
+                for (int c = 0; c < 9; ) {
+                    for (int i = r; i < r + 3; i++) {
+                        for (int j = c; j < c + 3; j++) {
+                            if (board[i][j] != '.') {
+                                number = board[i][j] - '0';
+                                flag[number]++;
+                                if (flag[number] > 1)
+                                    return false;
+                            }
+                        }
+                    }
+                    flag = new int[10];
+                    c = c + 3;
+                }
+                r = r + 3;
+            }
+
+            return true;
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
--- src/leetcode/editor/cn/ID48RotateImage.java
+++ src/leetcode/editor/cn/ID48RotateImage.java
@@ -1,7 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.*;
-
 public class ID48RotateImage {
     public static void main(String[] args) {
         Solution solution = new ID48RotateImage().new Solution();
@@ -14,11 +12,33 @@ public class ID48RotateImage {
 
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        /*将旋转90°的操作变为转置+对称*/
         public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            if (n == 1) {
+                return;
+            }
 
+            int temp = 0;
+            // 1.将矩阵转置
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) { // 处理对角线的上半三角即可
+                    temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+            // 2.将矩阵元素左右对换
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n / 2; j++) { // 处理中轴左边
+                    temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][n - 1 - j];
+                    matrix[i][n - 1 - j] = temp;
+                }
+            }
             return;
         }
     }
-// leetcode submit region end(Prohibit modification and deletion)
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
