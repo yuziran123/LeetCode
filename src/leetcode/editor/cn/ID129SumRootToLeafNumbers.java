0705bbbc3e69c6370c75adc45a74f37aca0540fd
yu <yu@example.com>
2024-09-11 19:37:55 +0800

    School update commit 二叉树

--- src/leetcode/editor/cn/ID129SumRootToLeafNumbers.java
+++ src/leetcode/editor/cn/ID129SumRootToLeafNumbers.java
@@ -17,33 +17,57 @@ public class ID129SumRootToLeafNumbers {
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         /**
-         * 描述:用list将每条路径以字符存储——再遍历循环转数字求和即可
+         * 描述:pathSum记录当前路径和，每下一层*10，返回上一层/10；遇到到叶子节点加入总和
          * Date 2024/9/11
          */
-        List<String> list = new ArrayList<>();
-        StringBuilder str = new StringBuilder();
+        int sum = 0;
+        int pathSum = 0;
 
         public int sumNumbers(TreeNode root) {
-            int sum = 0;
             sum(root);
-            for (int i = 0; i < list.toArray().length; i++) {
-                sum += Integer.valueOf(list.get(i));
-            }
-            return sum;
+            return this.sum;
         }
 
         public void sum(TreeNode root) {
             if (root == null)
                 return;
-            str.append(root.val);
+            pathSum = pathSum * 10 + root.val;
             if (root.left == null && root.right == null)
-                list.add(str.toString());
+                sum += pathSum;
 
             sum(root.left);
             sum(root.right);
-            str.deleteCharAt(str.length() - 1);
+            pathSum = pathSum - root.val;
+            pathSum = pathSum / 10;
         }
     }
     // leetcode submit region end(Prohibit modification and deletion)
 
+    /**
+     * 描述:用list将每条路径以字符存储——再遍历循环转数字求和即可
+     * Date 2024/9/11
+     */
+    List<String> list = new ArrayList<>();
+    StringBuilder str = new StringBuilder();
+
+    public int sumNumbers(TreeNode root) {
+        int sum = 0;
+        sum(root);
+        for (int i = 0; i < list.toArray().length; i++) {
+            sum += Integer.valueOf(list.get(i));
+        }
+        return sum;
+    }
+
+    public void sum(TreeNode root) {
+        if (root == null)
+            return;
+        str.append(root.val);
+        if (root.left == null && root.right == null)
+            list.add(str.toString());
+
+        sum(root.left);
+        sum(root.right);
+        str.deleteCharAt(str.length() - 1);
+    }
 }
