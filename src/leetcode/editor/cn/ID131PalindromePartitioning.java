1e0bc62067fd787047db453098d0d15a11acf91c
yu <yu@example.com>
2024-09-16 14:26:14 +0800

    Home Update commit 回溯

---
+++ src/leetcode/editor/cn/ID131PalindromePartitioning.java
@@ -0,0 +1,26 @@
+package leetcode.editor.cn;
+
+import java.util.List;
+
+public class ID131PalindromePartitioning {
+    public static void main(String[] args) {
+        Solution solution = new ID131PalindromePartitioning().new Solution();
+
+        // 执行测试
+
+        System.out.println("");
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        List<List<String>> ans;
+
+        public List<List<String>> partition(String s) {
+
+
+            return ans;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
