bd219d37ce0ef4990a5cdfb46e3018447abe3231
yu <yu@example.com>
2024-09-21 16:04:02 +0800

    School Update 图

--- src/leetcode/editor/cn/ID133CloneGraph.java
+++ src/leetcode/editor/cn/ID133CloneGraph.java
@@ -1,44 +1,48 @@
 package leetcode.editor.cn;
 
-import leetcode.editor.util.TreeNode;
-import org.w3c.dom.Node;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ID133CloneGraph{
-	public static void main(String[] args) {
-		Solution solution = new ID133CloneGraph().new Solution();
-		StringBuilder builder = new StringBuilder();
-		
-		//执行测试
-		
-		System.out.println(builder);		
-	}
-//leetcode submit region begin(Prohibit modification and deletion)
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
+public class ID133CloneGraph {
+    public static void main(String[] args) {
+        Solution solution = new ID133CloneGraph().new Solution();
+        StringBuilder builder = new StringBuilder();
 
-class Solution {
-    public Node cloneGraph(Node node) {
+        // 执行测试
 
-		return null;
+        System.out.println(builder);
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+
+
+    class Solution {
+        public Node cloneGraph(Node node) {
+
+            return null;
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
--- src/leetcode/editor/cn/ID210CourseScheduleIi.java
+++ src/leetcode/editor/cn/ID210CourseScheduleIi.java
@@ -1,23 +1,22 @@
 package leetcode.editor.cn;
 
-import leetcode.editor.util.TreeNode;
+public class ID210CourseScheduleIi {
+    public static void main(String[] args) {
+        Solution solution = new ID210CourseScheduleIi().new Solution();
+        StringBuilder builder = new StringBuilder();
 
-public class ID210CourseScheduleIi{
-	public static void main(String[] args) {
-		Solution solution = new ID210CourseScheduleIi().new Solution();
-		StringBuilder builder = new StringBuilder();
-		
-		//执行测试
-		
-		System.out.println(builder);		
-	}
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // 执行测试
 
-		return null;
+        System.out.println(builder);
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+            return null;
+        }
+    }
+	// leetcode submit region end(Prohibit modification and deletion)
 
 }
