914d7e10a5325579272bc8e93a038625a0a8b859
yu <yu@example.com>
2024-09-26 14:31:42 +0800

    School Update-动态规划

--- src/leetcode/editor/cn/ID198HouseRobber.java
+++ src/leetcode/editor/cn/ID198HouseRobber.java
@@ -1,22 +1,19 @@
 package leetcode.editor.cn;
 
-import leetcode.editor.util.TreeNode;
-
-public class ID198HouseRobber{
-	public static void main(String[] args) {
-		Solution solution = new ID198HouseRobber().new Solution();
-		StringBuilder builder = new StringBuilder();
-		
-		//执行测试
-		
-		System.out.println(builder);		
-	}
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int rob(int[] nums) {
-		return 0;
+public class ID198HouseRobber {
+    public static void main(String[] args) {
+        Solution solution = new ID198HouseRobber().new Solution();
+        StringBuilder builder = new StringBuilder();
+        // 执行测试
+        System.out.println(builder);
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int rob(int[] nums) {
+            return 0;
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
--- src/leetcode/editor/cn/ID70ClimbingStairs.java
+++ src/leetcode/editor/cn/ID70ClimbingStairs.java
@@ -3,18 +3,24 @@ package leetcode.editor.cn;
 public class ID70ClimbingStairs {
     public static void main(String[] args) {
         Solution solution = new ID70ClimbingStairs().new Solution();
-
         // 执行测试
-
         System.out.println("");
     }
 
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int climbStairs(int n) {
-            return 0;
+            if (n == 1 || n == 2)
+                return n;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 2;
+            for (int i = 2; i < n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n - 1];
         }
     }
-	// leetcode submit region end(Prohibit modification and deletion)
+    // leetcode submit region end(Prohibit modification and deletion)
 
 }
