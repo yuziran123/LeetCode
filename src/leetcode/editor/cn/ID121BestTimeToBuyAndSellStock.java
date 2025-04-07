e29f786d42a5a6b68c55f1f3c7119fe6944ea4de
yu <yu@example.com>
2025-04-07 20:42:39 +0800

    hot 100 day6 贪心算法

---
+++ src/leetcode/editor/cn/ID121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,21 @@
+package leetcode.editor.cn;
+
+public class ID121BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        Solution solution = new ID121BestTimeToBuyAndSellStock().new Solution();
+        StringBuilder builder = new StringBuilder();
+        // 执行测试
+
+        System.out.println(builder);
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int ans = 0;
+            return 0;
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/ID763PartitionLabels.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ID763PartitionLabels {
+    public static void main(String[] args) {
+        Solution solution = new ID763PartitionLabels().new Solution();
+        StringBuilder builder = new StringBuilder();
+        // 执行测试
+
+        System.out.println(builder);
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<Integer> partitionLabels(String s) {
+            List<Integer> ans = new ArrayList<>();
+            return ans;
+        }
+    }
+	// leetcode submit region end(Prohibit modification and deletion)
+
+}
