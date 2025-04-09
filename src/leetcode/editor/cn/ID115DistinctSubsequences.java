5b876c1b5cea5df4e7d2e21e5c1184cf18be5f66
yu <yu@example.com>
2025-04-09 08:46:48 +0800

    不同子序列

---
+++ src/leetcode/editor/cn/ID115DistinctSubsequences.java
@@ -0,0 +1,20 @@
+package leetcode.editor.cn;
+
+public class ID115DistinctSubsequences {
+    public static void main(String[] args) {
+        Solution solution = new ID115DistinctSubsequences().new Solution();
+        StringBuilder builder = new StringBuilder();
+        // 执行测试
+
+        System.out.println(builder);
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numDistinct(String s, String t) {
+            return 0;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
