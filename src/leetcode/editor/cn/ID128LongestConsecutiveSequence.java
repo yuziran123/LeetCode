306c04c600561feebf099be198f5683ec37a3e1d
yu <yu@example.com>
2025-04-15 10:07:01 +0800

    不同子序列

--- src/leetcode/editor/cn/ID128LongestConsecutiveSequence.java
+++ src/leetcode/editor/cn/ID128LongestConsecutiveSequence.java
@@ -6,7 +6,6 @@ public class ID128LongestConsecutiveSequence {
     public static void main(String[] args) {
         Solution solution = new ID128LongestConsecutiveSequence().new Solution();
         StringBuilder builder = new StringBuilder();
-
         // 执行测试
         int[] nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
         solution.longestConsecutive(nums);
@@ -15,15 +14,15 @@ public class ID128LongestConsecutiveSequence {
 
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        //[1,1,2,2] = 2
         public int longestConsecutive(int[] nums) {
             int ans = 0;
             HashSet<Integer> set = new HashSet<>();
             for (int num : nums) {
                 set.add(num);
             }
-            // nums转成set 去重
             for (int x : set) {
+                // 如果当前元素 x 的前一个数 x-1 存在于集合中，则说明 x 不是一个连续序列的起点
+                // 跳出此次循环，避免重复计算
                 if (set.contains(x - 1)) {
                     continue;
                 }
@@ -38,5 +37,4 @@ public class ID128LongestConsecutiveSequence {
         }
     }
     // leetcode submit region end(Prohibit modification and deletion)
-
 }
